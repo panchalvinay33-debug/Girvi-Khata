eacbc5d4990f81f6e44873a4f34df0d00597a4f8
panchalvinay33-debug <panchalvinay33@gmail.com>
2026-07-30 00:33:20 +0530

    Bump testing build to Alpha 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.girvikhata.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.9.0"
+        versionCode = 10
+        versionName = "0.10.0"
         manifestPlaceholders["appLabel"] = "Girvi Khata"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
