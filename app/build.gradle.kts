d4ba4a9ed4025bb36839a2c062d4727c2463b6c9
panchalvinay33-debug <panchalvinay33@gmail.com>
2026-07-30 14:56:19 +0530

    Bump testing version to Alpha 18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.girvikhata.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.17.0"
+        versionCode = 18
+        versionName = "0.18.0"
         manifestPlaceholders["appLabel"] = "Girvi Khata"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
