e776fb3562dcc88ea88d1a349b388390cd432bf0
panchalvinay33-debug <panchalvinay33@gmail.com>
2026-08-01 16:42:51 +0530

    Bump Alpha 25B testing build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.girvikhata.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.24.0"
+        versionCode = 25
+        versionName = "0.25.1"
         manifestPlaceholders["appLabel"] = "Girvi Khata"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
