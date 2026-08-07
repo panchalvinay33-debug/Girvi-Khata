9579dd0c4791e3ec6d36a7ef6eca52f92dc72ec5
panchalvinay33-debug <panchalvinay33@gmail.com>
2026-08-07 17:48:42 +0530

    Bump testing version to 0.28.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.girvikhata.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 33
-        versionName = "0.28.0"
+        versionCode = 34
+        versionName = "0.28.1"
         manifestPlaceholders["appLabel"] = "Girvi Khata"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
