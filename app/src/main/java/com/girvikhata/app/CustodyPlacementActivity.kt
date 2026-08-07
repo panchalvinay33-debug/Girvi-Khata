351a1835c4c4d3fe408d652e1ab743806a240e6b
panchalvinay33-debug <panchalvinay33@gmail.com>
2026-08-07 15:47:05 +0530

    Fix custody Compose weight import

--- app/src/main/java/com/girvikhata/app/CustodyPlacementActivity.kt
+++ app/src/main/java/com/girvikhata/app/CustodyPlacementActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -53,7 +54,6 @@ import androidx.compose.ui.unit.sp
 import androidx.fragment.app.FragmentActivity
 import com.girvikhata.app.custody.CustodyPlacementSnapshot
 import com.girvikhata.app.custody.CustodyPlacementStore
-import com.girvikhata.app.custody.ExternalParty
 import com.girvikhata.app.custody.PlacementLot
 import com.girvikhata.app.custody.StorageLocation
 import com.girvikhata.app.data.AppSnapshot
